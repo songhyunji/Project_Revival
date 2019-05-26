8a500ffda114631df1599cc059f87567aa6198a8
YeonWooHwang <50245330+YeonWooHwang@users.noreply.github.com>
2019-05-26 21:51:44 +0900

    button interactable error
    error

--- unity revival/Assets/_Script/ButtonInteractable.cs
+++ unity revival/Assets/_Script/ButtonInteractable.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class ButtonInteractable : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Toggle small11, small12, small21, small22, small31, small32, small41, small42;
+    public Button btn;
+
+   
     void Start()
     {
-        
+        btn = GetComponent<Button>();
+        small11 = GetComponent<Toggle>();
+        small12 = GetComponent<Toggle>();
+        small21 = GetComponent<Toggle>();
+        small22 = GetComponent<Toggle>();
+        small31 = GetComponent<Toggle>();
+        small32 = GetComponent<Toggle>();
+        small41 = GetComponent<Toggle>();
+        small42 = GetComponent<Toggle>();
+        btn.interactable = false;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if ((small11.isOn || small12.isOn)&& (small21.isOn || small22.isOn) && (small31.isOn || small32.isOn)&& (small41.isOn || small42.isOn))
+        {
+            btn.interactable = true;
+        }
+        else
+        {
+            btn.interactable = false;
+        }
     }
 }

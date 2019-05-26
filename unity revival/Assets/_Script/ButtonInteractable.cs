ac78b42ec648cb7f90405868a75b830182226f02
YeonWooHwang <50245330+YeonWooHwang@users.noreply.github.com>
2019-05-26 21:16:59 +0900

    Asset, Script
    옮기기

--- unity revival/Assets/_Script/ButtonInteractable.cs
+++ unity revival/Assets/_Script/ButtonInteractable.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonInterable : MonoBehaviour
+public class ButtonInteractable : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
